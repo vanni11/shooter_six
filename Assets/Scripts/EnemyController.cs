a28321b3d3793fa35c35bdc48793271bb792b1c3
Hoon <varick137@gmail.com>
2019-05-19 20:30:37 +0900

    적들 스폰되고 플레이어 따라오게 함

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+	NavMeshAgent nav;
+	GameObject player;
+
+	private void Awake()
+	{
+		player = GameObject.Find("Player");
+		nav = GetComponent<NavMeshAgent>();
+	}
+
+	private void Update()
+	{
+		nav.SetDestination(player.transform.position);
+	}
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemyPrefab;
+	public List<Transform> spawnPoints = new List<Transform>();
+
+	private void Start()
+	{
+		InvokeRepeating("Spawn", 1f, 3f);
+	}
+
+	private void Spawn()
+	{
+		int randomNum = Random.Range(0, 3);
+		Instantiate(enemyPrefab, spawnPoints[randomNum]);
+	}
+}
