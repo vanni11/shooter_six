ce0a32938afa470ee2ca0be834fa5bd6db51dce3
Hoon <varick137@gmail.com>
2019-05-22 21:33:18 +0900

    프리팹, 폴더명등 정리

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+	NavMeshAgent nav;
+	GameObject player;
+
+	private void Awake()
+	{
+		nav = GetComponent<NavMeshAgent>();
+		player = GameObject.Find("Player");
+	}
+
+	private void Update()
+	{
+		nav.SetDestination(player.transform.position);
+	}
+}
