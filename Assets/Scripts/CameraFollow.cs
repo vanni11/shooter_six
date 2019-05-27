da08961f4f94c088ea27045307435da1b7ac43d4
dahun2 <dyek73@gmail.com>
2019-05-28 00:10:59 +0900

    적 에셋 추가만 했고 맵 조명 좀 추가, 총에 맞아도 안죽음..

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,17 +7,17 @@ public class CameraFollow : MonoBehaviour {
 	public Transform target; // 추적할 타겟
 	public float smoothing = 2f; // 카메라가 움직이는 지연시간
 
-	Vector3 offset; // 카메라와 플레이어의 거리 동일한 거리를 유지하기 위함
+	Vector3 offset2; // 카메라와 플레이어의 거리 동일한 거리를 유지하기 위함
 
 	private void Start()
 	{
-		offset = transform.position - target.position;
+		offset2 = transform.position - target.position;
 	}
 
     // 플레이어는 FixedUpdate에서 움직인다.
 	private void FixedUpdate()
 	{
-		Vector3 targetCampos = target.position + offset;
+		Vector3 targetCampos = target.position + offset2;
 		transform.position = Vector3.Lerp(transform.position, targetCampos, smoothing * Time.deltaTime);
 	}
 }
